f117c5da6e686009db5f8ecbee32467e16472a5b
Nazorm <nnadoziechinazor@gmail.com>
2020-08-08 00:43:20 +0100

    fetched Api

--- find.js
+++ find.js
@@ -1,48 +1,83 @@
-let getList = document.querySelector(".dataList");  
+class Details {
+  constructor() {
+    this.items = [];
+  }
 
+  fetchData() {
+    var value = document.querySelector(".username");
+    var userValue = value.value;
+    fetch(`https://api.github.com/users/${userValue}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.items = data;
+        console.log(data);
+      });
+  }
 
-class Details{
-    constructor(items){
-     this.items = [items];
+  changeDisplay(userInfo) {
+    //let ul = document.querySelector(".datalist")
+
+    let newItem = document.createElement("li");
+    for (let i = 0; i < userInfo.length; i++) {
+      let itemText = document.createTextNode(userInfo[i]);
+      newItem.appendChild(itemText);
+      // ul.appendChild(newItem)
     }
-   
-   
-    fetchData(){
-        var value = document.querySelector(".username");
-      var userValue = value.value;
-     fetch(`https://api.github.com/users/${userValue}`)
-    .then((response) => response.json())
-    .then(function (data) {
-        console.log(data)
-})
-this.items = this.data
- }
+    //console.log(ul)
+    // console.log(newItem)
+  }
+  getUserRepo() {
+    this.items = "https://api.github.com/users/nazorm/following";
+    this.changeDisplay(this.items);
+    console.log(this.items);
+  }
 
-getUserRepo(){
-    console.log(this.items)
-//     let ul = document.getElementsByTagName("ul")
-//     let newItem = document.createElement("li")
-    
-// // for (let i = 0; i<=0; i++){
-//     let itemText = document.createTextNode("")
-//     newItem.appendChild(itemText)
-//     ul.appendChild(newItem)
-//     ul.appendChild("br")
-// // }
+  getUserGists() {
+    this.items = this.items.public_gists;
+    this.changeDisplay(this.items);
+  }
+
+  getUserFollowing() {
+    this.items = this.items.following;
+    this.changeDisplay(this.items);
+  }
+
+  getUserFollowers() {
+    this.items = this.items.followers;
+    this.changeDisplay(this.items);
+  }
+  leadToUserProfile() {
+    var value = document.querySelector(".username");
+    var userValue = value.value;
+    document.querySelector(".userP").href = `https://github.com/${userValue}`;
+  }
+  changeAvatar() {
+    document.querySelector(".userImage").src =
+      "https://avatars3.githubusercontent.com/u/31112419?v=4";
+  }
 }
 
-
-
-}
-
-var details = new Details
-
+var details = new Details();
 
 const getUserDetails = () => {
-    var catalog = document.querySelector(".dataCatalog");
-    catalog.style.display = "block";
-    details.fetchData()
-}
-const getUserRepo = ()=>{
-    details.getUserRepo()
-}
+  var catalog = document.querySelector(".dataCatalog");
+  catalog.style.display = "block";
+  details.fetchData();
+  details.leadToUserProfile();
+  details.changeAvatar();
+};
+const getUserRepo = () => {
+  details.getUserRepo();
+};
+
+const getUserGists = () => {
+  details.getUserGists();
+};
+
+const getUserFollowing = () => {
+  details.getUserFollowing();
+};
+
+const getUserFollowers = () => {
+  details.getUserFollowers();
+};
