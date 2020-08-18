40b5b0c3c12f46d14a36cf3e704592538e88a306
Nazorm <nnadoziechinazor@gmail.com>
2020-08-18 16:06:37 +0100

    committed

--- find.js
+++ find.js
@@ -1,12 +1,15 @@
 var generalItemList = document.querySelector(".generalDataList");
 var specificdataList = document.querySelector(".specificdataList");
 var userRepo = document.querySelector(".userRepo");
-var profileName = document.querySelector(".username");
+var profileId = document.querySelector(".username");
+var catalog = document.querySelector(".dataCatalog");
 
 class Details {
   constructor() {
     this.items = [];
   }
+
+  // debounce
   debounce(fn, delay) {
     let timer;
     return function () {
@@ -17,6 +20,7 @@ class Details {
     };
   }
 
+  //fetch multiple users
   fetchData() {
     var value = document.querySelector(".username");
     var userValue = value.value;
@@ -26,9 +30,9 @@ class Details {
       .then((data) => {
         this.items = data.items;
         this.handleEveryUserDisplay(this.items);
-        // console.log(this.items);
       });
   }
+  //display multiple querries on screen
   handleEveryUserDisplay(items) {
     generalItemList.innerHTML = " ";
     for (let i = 0; i < items.length; i++) {
@@ -36,11 +40,11 @@ class Details {
         <img src='${items[i].avatar_url}' onclick="handleSpecificUserClick(${i})"/>
         <span>${items[i].login}</span>
         </li>`;
-
+        catalog.style.display = "none";
       generalItemList.innerHTML += item;
     }
   }
-
+//display specific user details to screen
   handleSpecificUser(itemIndex) {
     generalItemList.innerHTML = " ";
     let specificUser = this.items[itemIndex];
@@ -50,7 +54,6 @@ class Details {
       .then((res) => res.json())
       .then((data) => {
         let userInfo = data;
-        // console.log(userInfo)
 
         this.getUserRepo(userInfo);
         this.pasteUserInfo(userInfo);
@@ -60,7 +63,7 @@ class Details {
         document.querySelector(".userP").href = specificUser.html_url;
       });
   }
-
+//fetch user Repos
   getUserRepo(value) {
     fetch(value.repos_url)
       .then((res) => res.json())
@@ -69,13 +72,13 @@ class Details {
         this.displayRepo(repos);
       });
   }
-
+// display user repos to screen
   displayRepo(value) {
     for (let i = 0; i < value.length; i++) {
       var repo = `<li>
       <span><a href= "${value[i].html_url}" target="_blank">${value[i].name}</a></span>
       <span>Fork:${value[i].fork}</span>
-      <span>Watchers:${value[i].watchers}</span>
+      <span>Watchers:${value[i].watchers_count}</span>
       <span>Stars:${value[i].stargazers_count}</span>
       
       </li>`;
@@ -112,7 +115,6 @@ var details = new Details();
 
 const getUserDetails = () => {
   details.debounce(details.fetchData(), 3000);
-
   //details.fetchData()
 };
 
